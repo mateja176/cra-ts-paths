e810c9418aa3faaec5c918e42b5a537e0f4ae97b
Mateja Petrovic <ma.petrovic@levi9.com>
2019-05-16 15:07:09 +0200

    wraps App in hot

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
+import { capitalize } from 'format';
 import React from 'react';
-import logo from './logo.svg';
+import { hot } from 'react-hot-loader';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => <div className="App">{capitalize('react')}</div>;
 
-export default App;
+export default hot(module)(App);
