ccb153d266fcacf0727707786e35d83d4506527e
Mateja Petrovic <ma.petrovic@levi9.com>
2019-05-16 15:09:35 +0200

    adds alias to config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,14 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const path = require('path');
 
-module.exports = (config, env) => rewireReactHotLoader(config, env);
+module.exports = (config, env) =>
+  rewireReactHotLoader(
+    {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: { '': path.resolve(__dirname, 'lib') },
+      },
+    },
+    env,
+  );
